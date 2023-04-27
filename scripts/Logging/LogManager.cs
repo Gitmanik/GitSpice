36be1a23ed12fffd3158a7c823571c05c194c95d
Pawel Reich <thegitman@outlook.com>
2023-04-27 18:52:11 +0200

    Experimental Loki-based logging solution

--- scripts/Logging/LogManager.cs
+++ scripts/Logging/LogManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Godot;
 using NLog;
 
@@ -14,7 +15,7 @@ namespace Gitmanik.Logging.Godot
         private void ConfigNLog()
         {
             NLog.Config.LoggingConfiguration nlog_config = new NLog.Config.LoggingConfiguration();
-            NLog.Targets.FileTarget nlog_logfile = new NLog.Targets.FileTarget("logfile")
+            NLog.Targets.FileTarget nlog_logfile = new NLog.Targets.FileTarget()
             {
                 Layout = "${longdate}\t${level:uppercase=true}\t${logger}\t${message:withexception=true}",
                 FileName = "Logs/Logs.txt",
@@ -24,11 +25,23 @@ namespace Gitmanik.Logging.Godot
                 ArchiveFileName = "Logs/Logs.{##}.txt",
             };
 
-            NLog.Targets.ColoredConsoleTarget nlog_logconsole = new NLog.Targets.ColoredConsoleTarget("logconsole")
+            NLog.Targets.ColoredConsoleTarget nlog_logconsole = new NLog.Targets.ColoredConsoleTarget()
             {
                 Layout = "${date:format=HH\\:mm\\:ss} ${level:uppercase=true}\t${logger:long=True}: ${message} ${exception:format=message}"
             };
 
+            NLog.Loki.LokiTarget nlog_loki = new NLog.Loki.LokiTarget()
+            {
+                Endpoint = "http://localhost:3100",
+                OrderWrites = false,
+                Username = "gitspice",
+                Password = "0Vt7YelNav96FpK8SMz19GzM",
+                Layout = "${date:format=HH\\:mm\\:ss} ${level:uppercase=true}\t${logger:long=True}: ${message} ${exception:format=message}"
+            };
+
+            nlog_loki.Labels.Add(new NLog.Loki.LokiTargetLabel() { Name = "Gitcorp", Layout = "GitSpice" });
+
+            nlog_config.AddRule(LogLevel.Debug, LogLevel.Fatal, nlog_loki);
             nlog_config.AddRule(LogLevel.Debug, LogLevel.Fatal, NLogGodotTarget.GenerateTarget());
             nlog_config.AddRule(LogLevel.Debug, LogLevel.Fatal, nlog_logfile);
             nlog_config.AddRule(LogLevel.Info, LogLevel.Fatal, nlog_logconsole);
